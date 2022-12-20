1bdba4334cd15e4789ff3177c3285fd4d1473684
Renee Mumbi <1723renee@gmail.com>
2022-12-20 20:35:06 +0300

    Create a variable to store the baseUrl

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-fetchingMeals()
+// document.addEventListener("DOMContentLoaded", ()=>{
 
-})
 
+// })
 const baseUrl="https://www.themealdb.com/api/json/v1/1/search.php?s="
-
-function fetchingMeals(){
-    fetch (baseUrl)
-    .then((response)=>response.json())
-    .then ((meals)=>{
-        //console.log(meals)
-        //createMeals(meals)
-    })
-}
-
-function createMeals(meal) {
-    const mealSummary= meals.map((meal) => meal.strMeal)
-    console.log (createMeals(meal))
-}
+fetchingMeals()
